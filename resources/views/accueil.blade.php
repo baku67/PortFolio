426869cd5c0b62eba123d8aa8b8b6f84dcbd7af9
Basile <basile08@hotmail.fr>
2022-04-11 21:18:26 +0200

    wip

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -24,7 +24,7 @@
 
     <body>
 
-        <div style="width:100%; height:10%; ">
+        <div style="width:100%;">
             <h1 style="margin-left:4%; color:#ef3b2d;">Portfolio <span style="color:#E7E8F2; position:relative; top:7px;">&#9642;</span></h1>
             </br>
             <h2 style="margin-left:6%; color:#ef3b2d;">Basile KUNTZ <span style="color:#E7E8F2; font-size:80%;"> -  Développeur Web</span></h2>
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,5 @@ Route::get('/welcome', function () {
 });
 
 Route::get('/', function () {
-    return view('acceuil');
+    return view('accueil');
 });
