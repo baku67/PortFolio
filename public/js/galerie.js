b0be5ea48c36f43a7a42a98c330381fbe221b85a
baku67 <basile08@hotmail.fr>
2023-02-17 00:25:21 +0100

    Save before Fix H1 decalage

--- public/js/galerie.js
+++ public/js/galerie.js
@@ -1,6 +1,17 @@
 window.onload = function() {
 
 
+  document.getElementById("photographieTitle").addEventListener("click", function() {
+    document.querySelectorAll(".photosGallerie").forEach(function(elem) {
+      if (elem.style.display == "block") {
+        elem.style.opacity = "none";
+      }
+      else {
+        elem.style.opacity = "block";
+      }
+    });
+  })
+
 
     // Apparition des articles
     let articles = document.querySelectorAll("article");
