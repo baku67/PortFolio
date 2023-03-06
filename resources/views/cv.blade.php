a8190e323869b9e8ae5daa732320ac25ee6632c7
baku67 <basile08@hotmail.fr>
2023-03-06 23:46:02 +0100

    Bouton dl CV.pdf resp

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -82,7 +82,9 @@
         <!-- Contenu Page -->
         <div id="pageContainer">
 
-            <a href="img/CV_majed.pdf" download="CV_basileKuntz"><i class="fa-solid fa-file-arrow-down"></i>Télécharger en PDF</a><h2 id="titleCv" class="pageTitle cvTitle">Mon &nbsp;<span style="letter-spacing:3px;">CV</span></h2>
+            <div>
+                <h2 id="titleCv" class="cvTitle"><a href="img/CV_majed.pdf" download="CV_basileKuntz" id="dlCv"><i class="fa-solid fa-file-arrow-down"></i>Télécharger en PDF</a><span id="underlineCV">Mon &nbsp;<span style="letter-spacing:3px;">CV</span></span></h2>
+            </div>
 
             <!-- </br>
 
