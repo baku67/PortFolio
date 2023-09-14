4110826f54ff6cef2eecff7b86feab8f298092b8
baku67 <basile08@hotmail.fr>
2023-09-14 19:48:15 +0200

    "En recherche d'alternance CDA"

--- resources/views/projets.blade.php
+++ resources/views/projets.blade.php
@@ -56,7 +56,7 @@
             <div style="width:100%;">
                 <h1 class="testH1"><a href="/">Portfolio <span style="color:#E7E8F2; position:relative; top:6px;">&#9642;</span></a></h1>
                 <span  id="lineBreakMobile"></br></span>
-                <h2 id="portfolioSousTitre" style="margin-left:6%; color:#ef3b2d; font-size:1.2em;">Basile KUNTZ <span style="color:#E7E8F2; font-size:80%;"> -  Développeur Web <span id="lineBreakMobileStatus"></br></span><span id="status"> En formation</span><span id="status2"> En recherche de stage</span></span></h2>
+                <h2 id="portfolioSousTitre" style="margin-left:6%; color:#ef3b2d; font-size:1.2em;">Basile KUNTZ <span style="color:#E7E8F2; font-size:80%;"> -  Développeur Web <span id="lineBreakMobileStatus"></br></span><span id="status"> En formation</span><span id="status2"> En recherche d'alternance CDA</span></span></h2>
             </div>
 
             <!-- Ancien header  -->
