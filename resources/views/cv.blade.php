6c14adc32f4b9c3b523c37e6100ebfabd105209b
baku67 <basile08@hotmail.fr>
2024-01-23 20:48:02 +0100

    Ajout stage et icon FA /cv

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -126,11 +126,13 @@
 
                     <ul>
                         <li>2023: Formation <span class="highlightSpan">"Développeur web et web mobile"</span> à Élan Formation Strasbourg (validé)
+                            <br>
+                            <i class="fa-solid fa-arrow-right" style="margin-right:4px;"></i> <span class="highlightSpan">Stage</span> chez Hoplunch (Oberhausbergen)
                         </li>
                         <div class="separateurListe light"></div>
                         <li>2020-2022: <span class="highlightSpan">BTS SIO</span> - Option <span class="highlightSpan">SLAM</span> (<span class="highlightSpan">S</span>olutions <span class="highlightSpan">L</span>ogicielles et <span class="highlightSpan">A</span>pplications <span class="highlightSpan">M</span>étiers) <br>CCI campus Illkirch
                             </br>
-                            <span class="highlightSpan">Alternance</span> chez Europe Tech Group (Haguenau)
+                            <i class="fa-solid fa-arrow-right" style="margin-right:4px;"></i> <span class="highlightSpan">Alternance</span> chez Europe Tech Group (Haguenau)
                         </li>
                         <div class="separateurListe light"></div>
                         <li>2019-2020: <span class="highlightSpan">Service Civique</span> (Inclusion numérique)</li>
